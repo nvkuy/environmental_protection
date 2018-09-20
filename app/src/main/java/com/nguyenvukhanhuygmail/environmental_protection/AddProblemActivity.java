f6e389b86b35102d37c93b8b1e0ad12016b8d144
Your Name <nguyenvukhanhuy@gmail.com>
2018-09-20 08:36:17 +0700

    Complete take photo

--- app/src/main/java/com/nguyenvukhanhuygmail/environmental_protection/AddProblemActivity.java
+++ app/src/main/java/com/nguyenvukhanhuygmail/environmental_protection/AddProblemActivity.java
@@ -1,10 +1,10 @@
 package com.nguyenvukhanhuygmail.environmental_protection;
 
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
+import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -18,7 +18,10 @@ import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.ui.PlacePicker;
 import com.nguyenvukhanhuygmail.environmental_protection.adapter.Problem_image_adapter;
+import com.nguyenvukhanhuygmail.environmental_protection.ultil.RecyclerItemClickListener;
 
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AddProblemActivity extends AppCompatActivity {
@@ -30,7 +33,9 @@ public class AddProblemActivity extends AppCompatActivity {
 
     Problem_image_adapter problem_image_adapter;
 
-    List<byte[]> image_problem;
+    List<byte[]> image_problem = new ArrayList<>();
+
+    private int index = 0;
 
     private final int PLACE_PICKER_REQUEST = 1, IMAGE_PICKER_REQUEST = 2, IMAGE_TAKER_REQUEST = 3;
 
@@ -40,19 +45,32 @@ public class AddProblemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_problem);
         start();
         onButtonClick();
+        onImageProblemClick();
+    }
+
+    private void onImageProblemClick() {
+        rv_image_problem.addOnItemTouchListener(new RecyclerItemClickListener(getApplication(), new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                index = position;
+                PickOrTakeDialog(
+                        "Thay đổi ảnh"
+                );
+            }
+        }));
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode == RESULT_OK) {
+        if (resultCode == RESULT_OK && data != null) {
             if (requestCode == PLACE_PICKER_REQUEST) {
                 tv_location.setText(PlacePicker.getPlace(data, this).getName());
             } else if (requestCode == IMAGE_PICKER_REQUEST) {
-
+                addImageToRV(data);
             } else if (requestCode == IMAGE_TAKER_REQUEST) {
-
+                addImageToRV(data);
             } else {
 
             }
@@ -60,25 +78,69 @@ public class AddProblemActivity extends AppCompatActivity {
 
     }
 
-    public static class PickOrTakeDialogFragment extends DialogFragment {
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the Builder class for convenient dialog construction
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setMessage(R.string.PickOrTakeDialogMsg)
-                    .setPositiveButton(R.string.TakeButton, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-
-                        }
-                    })
-                    .setNegativeButton(R.string.PickButton, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-
-                        }
-                    });
-            // Create the AlertDialog object and return it
-            return builder.create();
+    private void addImageToRV(Intent data) {
+        Bitmap bmp = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] image = stream.toByteArray();
+        bmp.recycle();
+        if (index != image_problem.size()) {
+            image_problem.set(index, image);
+        } else {
+            image_problem.add(image);
         }
+        problem_image_adapter = new Problem_image_adapter(image_problem);
+        rv_image_problem.setAdapter(problem_image_adapter);
+    }
+
+    private void takePhoto(int code) {
+
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i, code);
+
+    }
+
+    private void choosePhoto(int code) {
+
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        i.setType("image/*");
+        startActivityForResult(i, code);
+
+    }
+
+    private void showMapPicker(int code) {
+
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        try {
+            startActivityForResult(builder.build(AddProblemActivity.this), code);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void PickOrTakeDialog(String title) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertDialogCustom);
+        builder.setTitle(title);
+        builder.setMessage("Chọn từ thiết bị hay chụp ảnh?");
+        builder.setCancelable(true);
+        builder.setPositiveButton("Chọn ảnh", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                choosePhoto(IMAGE_PICKER_REQUEST);
+            }
+        });
+        builder.setNegativeButton("Chụp ảnh", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                takePhoto(IMAGE_TAKER_REQUEST);
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
     }
 
     private void onButtonClick() {
@@ -86,21 +148,21 @@ public class AddProblemActivity extends AppCompatActivity {
         tv_location.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
-                try {
-                    startActivityForResult(builder.build(AddProblemActivity.this), PLACE_PICKER_REQUEST);
-                } catch (GooglePlayServicesRepairableException e) {
-                    e.printStackTrace();
-                } catch (GooglePlayServicesNotAvailableException e) {
-                    e.printStackTrace();
-                }
+                showMapPicker(PLACE_PICKER_REQUEST);
             }
         });
 
         add_image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                try {
+                    index = image_problem.size();
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                }
+                PickOrTakeDialog(
+                        "Thêm ảnh cho vấn đề"
+                );
             }
         });
 
@@ -119,13 +181,11 @@ public class AddProblemActivity extends AppCompatActivity {
 
         rv_image_problem = (RecyclerView) findViewById(R.id.rv_image_problem);
         rv_image_problem.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        problem_image_adapter = new Problem_image_adapter(image_problem);
-        rv_image_problem.setAdapter(problem_image_adapter);
 
         header_field = (EditText) findViewById(R.id.problem_title);
         describe_field = (EditText) findViewById(R.id.describe_problem);
         tv_location = (TextView) findViewById(R.id.place_picker_problem);
-        add_image = (TextView) findViewById(R.id.add_problem);
+        add_image = (TextView) findViewById(R.id.image_picker_problem);
         submit_button = (Button) findViewById(R.id.add_problem_button);
 
     }
